3a3e1ca1d1a519c42cc16b3c839349503fe1dc40
Abhishek <saxenaa218@gmail.com>
2022-08-17 13:32:02 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Saxenaa218/vue-primevue-poc.git main:gh-pages
+git push -f git@github.com:Saxenaa218/vue-primevue-poc.git master:gh-pages
 cd -
